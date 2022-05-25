0975296fb6b26cc3fa47eccd77f0c05067519bce
Venkatesh Muppuri <venkatesh.muppuri@criticalriver.com>
2022-05-25 11:24:11 +0530

    - Updated Files

---
+++ CRPermissions/CRPermissions.h
@@ -0,0 +1,18 @@
+//
+//  CRPermissions.h
+//  CRPermissions
+//
+//  Created by Venkatesh Muppuri on 09/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRPermissions.
+FOUNDATION_EXPORT double CRPermissionsVersionNumber;
+
+//! Project version string for CRPermissions.
+FOUNDATION_EXPORT const unsigned char CRPermissionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRPermissions/PublicHeader.h>
+
+
